934a818860c43c8c5a223129695bb93a83d515dc
metasebiyaw asfaw <metasebiyawasfaw@gmail.com>
2025-02-28 10:54:00 +0300

    update

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
+// const dotenv = require('dotenv')
 const app = express();
 
+// dotenv.config()
+
 // connect MongoDB
-mongoose.connect('mongodb+srv://metasebiyawasfaw:awt@clusterkal.e4bll.mongodb.net/?retryWrites=true&w=majority&appName=Clusterkal', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://metasebiyawasfaw:awt@clusterkal.e4bll.mongodb.net/?retryWrites=true&w=majority&appName=Clusterkal',
+//  { useNewUrlParser: true, useUnifiedTopology: true }
+)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Could not connect to MongoDB', err));
 
 //Import Routes
 const authRoute = require('./routes/auth');
 
-
-//Middleware
+// Middleware
+app.use(express.json()) 
+ 
+// Route Middleware
 app.use('/api/user', authRoute);
 
 app.listen(3000, () => console.log('Server is running on port 3000'));
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,32 @@
 const router = require('express').Router();
+const User = require('../models/User');
+const { registerValidation, loginValidation } = require('../validation');
 
-router.post('/register', (req, res) => {
-    res.send('Register');
+router.post('/register', async (req, res) => {
+    // Validate the data before creating a user
+    const { error } = registerValidation(req.body);
+    if (error) {
+        const errorDetails = error.details.map(detail => detail.message);
+        return res.status(400).send(errorDetails);
+    }
+
+    // Check if the user already exists
+    const emailExists = await User.findOne({ email: req.body.email });
+    if (emailExists) return res.status(400).send('Email already exists');
+
+    // Create a new user
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    try {
+        const savedUser = await user.save();
+        res.send(savedUser);
+    } catch (err) {
+        res.status(400).send(err);
+    }
 });
 
 module.exports = router; 
