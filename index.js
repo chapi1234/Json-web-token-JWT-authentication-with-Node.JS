159d2b94e741fb6464b5000f68c59c691aa5b4a1
metasebiyaw asfaw <metasebiyawasfaw@gmail.com>
2025-02-28 17:46:14 +0300

    update

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
-// const dotenv = require('dotenv')
-const app = express();
+const dotenv = require('dotenv');
 
-// dotenv.config()
+// Import Routes
+const authRoute = require('./routes/auth');
+const postRoute = require('./routes/posts');
+
+dotenv.config(); // Load environment variables
+
+const app = express();
+const MONGO_DB = process.env.MONGO_DB;
 
 // connect MongoDB
-mongoose.connect('mongodb+srv://metasebiyawasfaw:awt@clusterkal.e4bll.mongodb.net/?retryWrites=true&w=majority&appName=Clusterkal',
-//  { useNewUrlParser: true, useUnifiedTopology: true }
-)
+mongoose.connect(MONGO_DB)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Could not connect to MongoDB', err));
 
-//Import Routes
-const authRoute = require('./routes/auth');
-
 // Middleware
-app.use(express.json()) 
- 
+app.use(express.json());
+
 // Route Middleware
 app.use('/api/user', authRoute);
+app.use('/api/posts', postRoute);
 
 app.listen(3000, () => console.log('Server is running on port 3000'));
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { registerValidation, loginValidation } = require('../validation');
+const dotenv = require('dotenv');
+dotenv.config();
 
 router.post('/register', async (req, res) => {
     // Validate the data before creating a user
@@ -14,19 +18,51 @@ router.post('/register', async (req, res) => {
     const emailExists = await User.findOne({ email: req.body.email });
     if (emailExists) return res.status(400).send('Email already exists');
 
+    // Hash the password 
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
     // Create a new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
     });
 
     try {
         const savedUser = await user.save();
-        res.send(savedUser);
+        res.send({
+            message: "succesfully registered",
+            user: savedUser
+        });
     } catch (err) {
         res.status(400).send(err);
     }
 });
 
-module.exports = router; 
+router.post('/login', async (req, res) => {
+    // Validate the data before logging in
+    const { error } = loginValidation(req.body);
+    if (error) {
+        const errorDetails = error.details.map(detail => detail.message);
+        return res.status(400).send(errorDetails);
+    }
+
+    // Check if the email exists
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send('Email is not found');
+
+    // Check if the password is correct
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if (!validPass) return res.status(400).send('Invalid password');
+
+
+    // Create and assign a token
+    const token = jwt.sign({ _id: user._id }, process.env.SECRET_KEY);
+    res.header('auth-token', token).send({
+        message: 'Logged in!',
+        token: token
+    });
+});
+
+module.exports = router;
